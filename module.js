5129e0fff38f7924d7739bf7fa08fbe34dfc47d7
Philip Weaver <simplygreatwork@gmail.com>
2023-09-12 09:09:43 -0500

    shove

---
+++ module.js
@@ -0,0 +1,2 @@
+
+export { add } from './source/core.js'
---
+++ source/core.js
@@ -0,0 +1,10 @@
+
+export function add(a, b) {
+	
+	return a + b
+	
+	//	Adds x and y.
+	// @param {number} x
+	// @param {number} y
+	// @returns {number} Sum of x and y
+}
